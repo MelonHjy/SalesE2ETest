9672f4a95dfb7222e528aa2152c2554900c82e07
冯玉柳 <fengyuliu@sinosoft.com.cn>
2020-06-30 17:53:32 +0800

    6.30

--- src/testcase/base_step/commonsteps.py
+++ src/testcase/base_step/commonsteps.py
@@ -14,15 +14,34 @@ class CommonSteps():
     #密码输入框
     pwd = "//input[@id='password1']"
     #登录按钮
-    login = "//input[@id='button']"
+    login_btn = "//input[@id='button']"
+
+    base_page = BasePage()
+    cons = Constants()
 
     def login_p(self):
-        base_page = BasePage()
-        cons = Constants()
-        username = base_page.wait_until_el_xpath(self.username)
-        base_page.send_keys(username, cons.JIANGSU_PROVINCE_USERNAME)
-        password = base_page.wait_until_el_xpath(self.pwd)
-        base_page.send_keys(password, cons.JIANGSU_PROVINCE_PASSWORD)
-        login = base_page.wait_until_el_xpath(self.login)
-        info("登录账号：{0}，密码：{1}".format(cons.JIANGSU_PROVINCE_USERNAME,cons.JIANGSU_PROVINCE_PASSWORD))
-        base_page.click(login)
+        username = self.base_page.wait_until_el_xpath(self.username)
+        self.base_page.send_keys(username, self.cons.JIANGSU_PROVINCE_USERNAME)
+        password = self.base_page.wait_until_el_xpath(self.pwd)
+        self.base_page.send_keys(password, self.cons.JIANGSU_PROVINCE_PASSWORD)
+        login_b = self.base_page.wait_until_el_xpath(self.login_btn)
+        info("登录账号：{0}，密码：{1}".format(self.cons.JIANGSU_PROVINCE_USERNAME, self.cons.JIANGSU_PROVINCE_PASSWORD))
+        self.base_page.click(login_b)
+
+    def login_c(self):
+        username = self.base_page.wait_until_el_xpath(self.username)
+        self.base_page.send_keys(username, self.cons.NANJING_CITY_USERNAME)
+        password = self.base_page.wait_until_el_xpath(self.pwd)
+        self.base_page.send_keys(password, self.cons.NANJING_CITY_PASSWORD)
+        login_b = self.base_page.wait_until_el_xpath(self.login_btn)
+        info("登录账号：{0}，密码：{1}".format(self.cons.NANJING_CITY_USERNAME, self.cons.NANJING_CITY_PASSWORD))
+        self.base_page.click(login_b)
+
+    def login(self):
+        username = self.base_page.wait_until_el_xpath(self.username)
+        self.base_page.send_keys(username, self.cons.NANJING_USERNAME)
+        password = self.base_page.wait_until_el_xpath(self.pwd)
+        self.base_page.send_keys(password, self.cons.NANJING_PASSWORD)
+        login_b = self.base_page.wait_until_el_xpath(self.login_btn)
+        info("登录账号：{0}，密码：{1}".format(self.cons.NANJING_USERNAME, self.cons.NANJING_PASSWORD))
+        self.base_page.click(login_b)
