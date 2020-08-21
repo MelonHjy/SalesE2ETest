6998b5a9088186f0e3e35cf7c94c8a190b8e4eab
冯玉柳 <fengyuliu@sinosoft.com.cn>
2020-08-21 20:18:12 +0800

    8.21 中介机构新增

--- execute.py
+++ execute.py
@@ -7,4 +7,5 @@ import pytest
 if __name__ == '__main__':
     # pytest.main(['-s', 'src/testcase/agent_sales_manage/test_SALES_validate.PY','-m','verify'])
     # pytest.main(['-s', 'src/testcase/agent_sales_manage/test_SALES_YLDLZ_200.PY'])
-    pytest.main(['-s', 'src/testcase/group_issue_manage/test_SALES_YLTD_001.PY'])
+    # pytest.main(['-s', 'src/testcase/group_issue_manage/test_SALES_YLTD_006.PY'])
+    pytest.main(['-s', 'src/testcase/agen_org_manage/test_SALES_YLZJ_004.PY'])
