7387882cf06e06bfebeb4f770e82db41ad88e33a
冯玉柳 <fengyuliu@sinosoft.com.cn>
2020-09-02 15:28:37 +0800

    9.2

--- src/testcase/agent_sales_manage/test_SALES_YLDLZ_002.py
+++ src/testcase/agent_sales_manage/test_SALES_YLDLZ_002.py
@@ -1,12 +1,11 @@
 # -*- coding:utf-8 -*-
 # @Time : 2020/6/28 17:22
 # @Author: fyl
-# @File : test_SALES_YLDLZ_001_.py   代理制人员代码管理>>营销团队经理聘任与解聘（有效团队成员任命为经理）
+# @File : _test_SALES_YLDLZ_001.py   代理制人员代码管理>>营销团队经理聘任与解聘（有效团队成员任命为经理）
 import allure
 import pytest
 
 from config.global_var import sleep
-from src.page.Personal_agency_channel.main_management_agent_sales_approve import ManagementAgentSalesApprove
 from src.page.Personal_agency_channel.sales_query import SalesQuery
 from src.page.agent_sales_manage.appointment_manager import AppointmentManager
 from src.page.agent_sales_manage.main_management_agent_salesmen import ManagementOfAgentSalesmen
